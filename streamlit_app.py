5aa5dc599c288e9360e6324702a2ceb4b02ddd41
Dhananjaya R <120083622+Dhananjaya-12@users.noreply.github.com>
2025-09-08 22:53:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 import pandas as pd
-import requests
 
 # Connect to Snowflake
 cnx = st.connection("snowflake")
@@ -20,8 +19,6 @@ st.write("The name on your Smoothie will be", name_on_order)
 # Get fruit options from Snowflake (with FRUIT_NAME + SEARCH_ON)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
 pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop
 
 # Multiselect for ingredients
 ingredients_list = st.multiselect(
@@ -30,26 +27,36 @@ ingredients_list = st.multiselect(
     max_selections=5    
 )
 
+# Only proceed if user picked something
+# if ingredients_list:
+#     ingredients_string = ", ".join(ingredients_list)
+#     st.write("Ingredients chosen:", ingredients_string)
 
+#     # Show nutrition only for valid fruits
+#     for fruit_chosen in ingredients_list:
+#         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+
+#         st.subheader(f"{fruit_chosen} Nutrition Information")
+
+#         if search_on == "N/A":
+#             st.info(f"{fruit_chosen} added to your smoothie! ✅ (No nutrition data available)")
+#         else:
+#             response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
+#             if response.status_code == 200:
+#                 st.dataframe(response.json(), use_container_width=True)
+#             else:
+#                 st.warning(f"Sorry, no data found for {fruit_chosen}")
 
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        #st.text(smoothiefroot_response.json())
-        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all" + search_on)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-        
 
     # Add a Submit button
     if st.button("Submit Order"):
@@ -61,6 +68,3 @@ if ingredients_list:
     
     
         st.success('Your Smoothie is ordered! ✅')
-
-
-
